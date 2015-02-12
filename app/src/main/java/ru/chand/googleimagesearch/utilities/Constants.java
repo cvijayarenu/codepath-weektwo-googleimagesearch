b8337f7cea79887d7d8c27ca437166db1c1b9672
chandrav <c@zoosk.com>
2015-02-11 23:57:53 -0800

    Added Toolbar

---
+++ app/src/main/java/ru/chand/googleimagesearch/utilities/Constants.java
@@ -0,0 +1,10 @@
+package ru.chand.googleimagesearch.utilities;
+
+/**
+ * Created by chandrav on 2/11/15.
+ */
+public class Constants {
+    
+  public final static String NUMBER_OF_RESULTS = "8";
+    
+}
